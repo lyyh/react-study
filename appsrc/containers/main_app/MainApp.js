c473034f91267a93357c8aee41d6e1aa6a039533
liuyanhao <374659635@qq.com>
2016-11-02 09:56:37 +0800

    init

---
+++ appsrc/containers/main_app/MainApp.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class MainApp extends React.Component{
+	render(){
+		return (
+			<h1>hello world</h1>
+		)
+	}
+}
+
+export default MainApp;
---
+++ appsrc/entry/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM,{render} from 'react-dom';
+import MainApp from '../containers/main_app/MainApp.js';
+
+render(
+		<MainApp/>,
+		document.getElementById("container")
+	)
